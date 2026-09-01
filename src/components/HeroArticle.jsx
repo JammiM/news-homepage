04da3b93b4801105ccda0bd341de92f5854bce91
James Morrissey <jammim19@gmail.com>
2026-09-01 12:28:08 +0100

    Added a basic DOM structure for the hero section

--- src/components/HeroArticle.jsx
+++ src/components/HeroArticle.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import heroArticleData from "../data/hero-data.json";
 
 function HeroArticle() {
-  return <div>HeroArticle</div>;
+  return (
+    <main>
+      <figure>
+        <img src={heroArticleData.image} alt="Hero Article" />
+      </figure>
+      <header>
+        <h1>{heroArticleData.title}</h1>
+      </header>
+      <p>{heroArticleData.description}</p>
+      <button>Read More</button>
+    </main>
+  );
 }
 
 export default HeroArticle;
